bff170edc0a73a968d7f2170c5720f644b21f7fa
itaji-create <itaji.1999@gmail.com>
2022-10-10 17:58:19 -0300

    feat: busca as informações das notícias para preencher um dicionário com atributos

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -32,7 +32,20 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    summary = selector.css(".entry-content > p:first-of-type *::text").getall()
+    summary = "".join(summary).strip()
+    noticia = {
+        "url": selector.css('head link[rel="canonical"]::attr(href)').get(),
+        "title": selector.css(".entry-title::text").get().strip(),
+        "timestamp": selector.css(".meta-date::text").get(),
+        "writer": selector.css(".author a::text").get(),
+        "comments_count": len(selector.css(".comment-body").getall()),
+        "category": selector.css(".category-style .label::text").get(),
+        "summary": summary,
+        "tags": selector.css(".post-tags ul li a::text").getall()
+    }
+    return noticia
 
 
 # Requisito 5
