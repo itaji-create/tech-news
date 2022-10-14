d239ea0ba2b795668dc2a9debc5993e2db614f25
itaji-create <itaji.1999@gmail.com>
2022-10-14 20:17:01 -0300

    feat: Esta função irá fazer buscas por título

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,10 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news_by_title = search_news({"title": {"$regex": title, "$options": 'i'}})
+    return [(new["title"], new["url"]) for new in news_by_title]
 
 
 # Requisito 7
