7fffd2dbcbcf7c5fe520dcae62f063f20ace3ddc
itaji-create <itaji.1999@gmail.com>
2022-10-18 17:14:39 -0300

    feat: cria menu do programa

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,14 @@
 # Requisito 12
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    options = input(
+        """Selecione uma das opções a seguir:
+ 0 - Popular o banco com notícias;
+ 1 - Buscar notícias por título;
+ 2 - Buscar notícias por data;
+ 3 - Buscar notícias por tag;
+ 4 - Buscar notícias por categoria;
+ 5 - Listar top 5 notícias;
+ 6 - Listar top 5 categorias;
+ 7 - Sair."""
+    )
+    return options
