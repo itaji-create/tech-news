03c5a4c291551fcab2613ba2a785fd4a40110a04
itaji-create <itaji.1999@gmail.com>
2022-10-18 12:15:33 -0300

    feat: Esta função irá listar as cinco notícias mais populares baseado no numeros de comentários

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,15 @@
+from tech_news.database import find_news
+
+
+def orderBy(e):
+    return e["comments_count"]
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    news.sort(key=orderBy, reverse=True)
+    return [(item["title"], item["url"]) for item in news][:5]
 
 
 # Requisito 11
